5bc9f517d3a7bfaf18873a301512fad929ec1351
newstore0077 <91786879+newstore0077@users.noreply.github.com>
2022-02-24 04:19:14 +0700

    Create avn.sh

---
+++ avn.sh
@@ -0,0 +1,8 @@
+# Donwload CPU Miner (SUGAR)
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.24/cpuminer-opt-linux.tar.gz
+ 
+# extrak file
+tar xf cpuminer-opt-linux.tar.gz
+ 
+# Run Mining
+./cpuminer-avx2 -a minotaurx -o stratum+tcp://stratum-eu.rplant.xyz:17068 -u RDjj8e6s8kVxkhSev5fu8MG3RFNmadUNc4.ph1 -p c=AVN
