48c341ec644f48c2df98deab54d7376cdd430238
newstore0077 <91786879+newstore0077@users.noreply.github.com>
2022-02-25 01:04:50 +0700

    Update gc.py

--- gc.py
+++ gc.py
@@ -6,27 +6,27 @@ password = ''.join(random.choice(string.ascii_letters + string.digits) for i in 
 
 #Download ngrok
 
-! wget -q -c -nc https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+wget -q -c -nc https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 
-! unzip -qq -n ngrok-stable-linux-amd64.zip
+unzip -qq -n ngrok-stable-linux-amd64.zip
 
 #Setup sshd
 
-! apt-get install -qq -o=Dpkg::Use-Pty=0 openssh-server pwgen > /dev/null
+apt-get install -qq -o=Dpkg::Use-Pty=0 openssh-server pwgen > /dev/null
 
 #Set root password
 
-! echo root:$password | chpasswd
+echo root:$password | chpasswd
 
-! mkdir -p /var/run/sshd
+mkdir -p /var/run/sshd
 
-! echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 
-! echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
+echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
 
-! echo "LD_LIBRARY_PATH=/usr/lib64-nvidia" >> /root/.bashrc
+echo "LD_LIBRARY_PATH=/usr/lib64-nvidia" >> /root/.bashrc
 
-! echo "export LD_LIBRARY_PATH" >> /root/.bashrc
+echo "export LD_LIBRARY_PATH" >> /root/.bashrc
 
 #Run sshd
 
