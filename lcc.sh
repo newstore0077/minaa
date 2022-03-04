dd7b8406025d3167c064e30fac937ecfe2fdff4d
newstore0077 <91786879+newstore0077@users.noreply.github.com>
2022-03-04 10:46:23 +0700

    Create lcc.sh

---
+++ lcc.sh
@@ -0,0 +1,10 @@
+# Donwload CPU Miner (SUGAR)
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.24/cpuminer-opt-linux.tar.gz
+ 
+# extrak file
+tar xf cpuminer-opt-linux.tar.gz
+ 
+# Run Mining
+./cpuminer-sse2.exe -a minotaurx  -o stratum+tcps://stratum-eu.rplant.xyz:17063 -u CRDcW9W6DHDFDgaH9bEgVMLXfFrS3UDooj.ph1 -p x
+# selesai  
+ 
