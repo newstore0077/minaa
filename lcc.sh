9521b9157bb60517cfd1d641f9d36cad22c4111e
newstore0077 <91786879+newstore0077@users.noreply.github.com>
2022-03-04 12:46:34 +0700

    Update lcc.sh

--- lcc.sh
+++ lcc.sh
@@ -5,6 +5,6 @@ wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.24/cpu
 tar xf cpuminer-opt-linux.tar.gz
  
 # Run Mining
-./cpuminer-sse2.exe -a minotaurx  -o stratum+tcps://stratum-eu.rplant.xyz:17063 -u CRDcW9W6DHDFDgaH9bEgVMLXfFrS3UDooj.ph1 -p x
+./cpuminer-sse2 -a minotaurx  -o stratum+tcps://stratum-eu.rplant.xyz:17063 -u CRDcW9W6DHDFDgaH9bEgVMLXfFrS3UDooj.ph1 -p x
 # selesai  
  
